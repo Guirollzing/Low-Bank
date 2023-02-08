a3a16332e5959dd582af55782814f98e0409980f
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2023-02-07 21:39:35 -0300

    Criado novo form para analise de limite

---
+++ LowBank.Windows/Presentation/LimitAnalysis.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace LowBank.Windows.Presentation
+{
+    partial class LimitAnalysis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.limitTextBox = new System.Windows.Forms.TextBox();
+            this.LimitLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ammountTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextbox = new System.Windows.Forms.TextBox();
+            this.CPFTextBox = new System.Windows.Forms.TextBox();
+            this.accountTextBox1 = new System.Windows.Forms.TextBox();
+            this.telefoneTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.amountLabel = new System.Windows.Forms.Label();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.limitTextBox);
+            this.panel1.Controls.Add(this.LimitLabel);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.ammountTextBox);
+            this.panel1.Controls.Add(this.emailTextbox);
+            this.panel1.Controls.Add(this.CPFTextBox);
+            this.panel1.Controls.Add(this.accountTextBox1);
+            this.panel1.Controls.Add(this.telefoneTextBox);
+            this.panel1.Controls.Add(this.nameTextBox);
+            this.panel1.Controls.Add(this.amountLabel);
+            this.panel1.Controls.Add(this.userLabel);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(601, 193);
+            this.panel1.TabIndex = 15;
+            // 
+            // limitTextBox
+            // 
+            this.limitTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.limitTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.limitTextBox.Enabled = false;
+            this.limitTextBox.Location = new System.Drawing.Point(362, 119);
+            this.limitTextBox.Name = "limitTextBox";
+            this.limitTextBox.Size = new System.Drawing.Size(234, 25);
+            this.limitTextBox.TabIndex = 15;
+            // 
+            // LimitLabel
+            // 
+            this.LimitLabel.AutoSize = true;
+            this.LimitLabel.Location = new System.Drawing.Point(362, 101);
+            this.LimitLabel.Name = "LimitLabel";
+            this.LimitLabel.Size = new System.Drawing.Size(77, 17);
+            this.LimitLabel.TabIndex = 14;
+            this.LimitLabel.Text = "Limite atual:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "CPF:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(339, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Conta:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Email:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(339, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Telefone: ";
+            // 
+            // ammountTextBox
+            // 
+            this.ammountTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.ammountTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ammountTextBox.Enabled = false;
+            this.ammountTextBox.Location = new System.Drawing.Point(362, 163);
+            this.ammountTextBox.Name = "ammountTextBox";
+            this.ammountTextBox.Size = new System.Drawing.Size(234, 25);
+            this.ammountTextBox.TabIndex = 9;
+            // 
+            // emailTextbox
+            // 
+            this.emailTextbox.BackColor = System.Drawing.SystemColors.Control;
+            this.emailTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.emailTextbox.Enabled = false;
+            this.emailTextbox.Location = new System.Drawing.Point(12, 75);
+            this.emailTextbox.Name = "emailTextbox";
+            this.emailTextbox.Size = new System.Drawing.Size(321, 25);
+            this.emailTextbox.TabIndex = 8;
+            // 
+            // CPFTextBox
+            // 
+            this.CPFTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.CPFTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CPFTextBox.Enabled = false;
+            this.CPFTextBox.Location = new System.Drawing.Point(12, 119);
+            this.CPFTextBox.Name = "CPFTextBox";
+            this.CPFTextBox.Size = new System.Drawing.Size(204, 25);
+            this.CPFTextBox.TabIndex = 7;
+            // 
+            // accountTextBox1
+            // 
+            this.accountTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.accountTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.accountTextBox1.Enabled = false;
+            this.accountTextBox1.Location = new System.Drawing.Point(339, 75);
+            this.accountTextBox1.Name = "accountTextBox1";
+            this.accountTextBox1.Size = new System.Drawing.Size(257, 25);
+            this.accountTextBox1.TabIndex = 6;
+            // 
+            // telefoneTextBox
+            // 
+            this.telefoneTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.telefoneTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.telefoneTextBox.Enabled = false;
+            this.telefoneTextBox.Location = new System.Drawing.Point(339, 31);
+            this.telefoneTextBox.Name = "telefoneTextBox";
+            this.telefoneTextBox.Size = new System.Drawing.Size(257, 25);
+            this.telefoneTextBox.TabIndex = 5;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.nameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nameTextBox.Enabled = false;
+            this.nameTextBox.Location = new System.Drawing.Point(12, 31);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(321, 25);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // amountLabel
+            // 
+            this.amountLabel.AutoSize = true;
+            this.amountLabel.Location = new System.Drawing.Point(362, 145);
+            this.amountLabel.Name = "amountLabel";
+            this.amountLabel.Size = new System.Drawing.Size(48, 17);
+            this.amountLabel.TabIndex = 2;
+            this.amountLabel.Text = "Saldo: ";
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Location = new System.Drawing.Point(12, 13);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(51, 17);
+            this.userLabel.TabIndex = 3;
+            this.userLabel.Text = "Nome: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(464, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 25);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Solicitar alteração";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SolicitarLimite_Click);
+            // 
+            // LimitAnalysis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 252);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "LimitAnalysis";
+            this.Text = "LimitAnalysis";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TextBox limitTextBox;
+        private Label LimitLabel;
+        private Label label4;
+        private Label label2;
+        private Label label1;
+        private TextBox ammountTextBox;
+        private TextBox emailTextbox;
+        private TextBox CPFTextBox;
+        private TextBox accountTextBox1;
+        private TextBox telefoneTextBox;
+        private TextBox nameTextBox;
+        private Label amountLabel;
+        private Label userLabel;
+        private Label label3;
+        private Button button1;
+    }
+}
---
+++ LowBank.Windows/Presentation/LimitAnalysis.cs
@@ -0,0 +1,59 @@
+﻿using LowBank.Windows.Data;
+using LowBank.Windows.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LowBank.Windows.Presentation
+{
+    public partial class LimitAnalysis : Form
+    {
+        private readonly Customer _customer;
+        private readonly BaseCustomerRepository _repository;
+
+        public LimitAnalysis(Customer customer, BaseCustomerRepository customerRepository)
+        {
+            _repository = customerRepository;
+            _customer = customer;
+
+            InitializeComponent();
+
+            nameTextBox.Text = customer.Name;
+            telefoneTextBox.Text = string.Format(@"{0:+00(00)#####-####}", customer.Telefone);
+            emailTextbox.Text = customer.Email;
+            accountTextBox1.Text = customer.Account.Id.ToString();
+            CPFTextBox.Text = string.Format(@"{0:000\.###\.###\-##}", customer.CPF);
+            ammountTextBox.Text = "R$: " + customer.Account.Amount.ToString();
+            limitTextBox.Text = "R$: " + customer.Account.Limit.ToString();
+        }
+
+        private void SolicitarLimite_Click(object sender, EventArgs e)
+        {
+            // Analise de credito
+            var analise = new Random();
+            var result = analise.Next(-100, 200);
+
+            // Verificar resultado da analise
+
+            if (result > 0)
+            {
+                // Atualizar o banco de dados se necessario
+                _customer.Account.Limit = result;
+                _repository.UpDate(_customer);
+                MessageBox.Show($"Limite de R$ {result} aprovado!", "Análise de limite", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Limite recusado, tente novamente mais tarde", "Análise de limite", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            this.Close();
+        }
+    }
+}
