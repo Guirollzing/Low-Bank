4c32a2f0cf03ccb6270c20044bda9a20e4cb14e8
Guilherme Rollzing Schineider <56838656+Guirollzing@users.noreply.github.com>
2023-02-14 14:08:58 -0300

    CustomerController
    Criado CustomerController.

---
+++ LowBank.Api/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using LowBank.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LowBank.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController
+    {
+        [HttpGet(Name = "GetCustomer")]
+
+        public IEnumerable<Customer> Get()
+        {
+            var customerList = new List<Customer>();
+
+            customerList.Add(new Customer("Guilherme", "guilherme@gmail.com", 00000000000, 51986387966, 1));
+
+            return customerList;
+        }
+
+    }
+}
---
+++ LowBank.Api/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿namespace LowBank.Api.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long CPF { get; set; }
+        public long Telefone { get; set; }
+        public Account Account { get; set; }
+        public int AccountId { get; set; }
+
+        public Customer(string name, string email, long cPF, long telefone, int accountId)
+        {
+            Name = name;
+            Email = email;
+            CPF = cPF;
+            Telefone = telefone;
+            AccountId = accountId;
+        }
+
+    }
+}
